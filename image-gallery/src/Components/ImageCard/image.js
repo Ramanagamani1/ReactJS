13c27ec25af2120d33224c14a0439ce8e3b70cff
Nagamani <ramanagamani1@gmail.com>
2020-09-06 12:49:25 +0530

    Add files via upload

---
+++ image-gallery/src/Components/ImageCard/image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+
+const ImageCard =(props)=>{
+    return(
+        <img alt="gallery pic" className="image-card" src={props.imageUrl}/>
+    )
+};
+export default ImageCard;
