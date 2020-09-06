e047cbd8fee29bc44935dff726a9fed05ea8a84d
Nagamani <ramanagamani1@gmail.com>
2020-09-06 12:57:42 +0530

    commit

---
+++ user-blog/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import UserGallery from './Components/UserGallery/index.js'
+import './App.css';
+
+function App() {
+  return (<UserGallery/>);
+}
+
+export default App;
